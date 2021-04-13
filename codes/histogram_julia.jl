6f88f641f712dcfd2121de734b58777ebf188cb4
r2rahul <katyayan.rahul@gmail.com>
2021-04-13 22:06:20 +0530

    added extra error tolerance in histogram code

--- codes/histogram_julia.jl
+++ codes/histogram_julia.jl
@@ -1,13 +1,15 @@
 #using Gadfly
 using Plots
 
+# %%
 function get_bin_edges(a, bins)
-    bin_edges = zeros(bins)
+    bin_edges = zeros(bins + 1)
+    k = size(bin_edges)[1]
     a_min = minimum(a)
     a_max = maximum(a)
     delta = (a_max - a_min) / bins
-    for i in 1:size(bin_edges)[1]
-        bin_edges[i] = a_min + i*delta
+    for i in 1:k
+        bin_edges[i] = a_min + i * delta
     end
 
     bin_edges[lastindex(bin_edges)] = a_max
@@ -15,6 +17,7 @@ function get_bin_edges(a, bins)
     return bin_edges
 end
 
+# %%
 function compute_bin(x, bin_edges)
     # assuming uniform bins for now
     n = size(bin_edges)[1]
@@ -35,14 +38,16 @@ function compute_bin(x, bin_edges)
     end
 end
 
-
+# %%
 function get_histogram(a, bins)
     hist = zeros(bins)
+    m = size(hist)[1]
     bin_edges = get_bin_edges(a, bins)
 
     for x in a[:]
         bin = compute_bin(x, bin_edges)
-        if !isnan(bin)
+        println(bin)
+        if !isnan(bin) && bin <= m && bin != 0
             hist[Int(floor(bin))] += 1
         end
     end
@@ -50,9 +55,10 @@ function get_histogram(a, bins)
     return hist, bin_edges
 end
 
+# %%
 #Test the Functions
 a = rand(100)
 h = get_histogram(a, 5)
 
-bar(h[1])
+bar(h[2], h[1])
 histogram(a, bins = 5)
