a5c8d9d9e99522c43ab279ce2ecdb6298350c1c1
Rahul Katyayan <katyayan.rahul@gmail.com>
2024-12-28 16:56:59 +0100

    modified F# to accept value from commandline

--- codes/exer_01_mc.fsx
+++ codes/exer_01_mc.fsx
@@ -1,23 +1,29 @@
 open System
-open System.IO
-// F# exercism on monte carlo method
-// read config file to get no of simualtion
 
-// set number of simulation
-let noOfSim = 1000000
+// Initialize the random number generator
 let seed = 42
 let random = Random(seed)
-let totalSim = seq {
-    for i in 1..noOfSim do
+
+// Define the function to perform simulation
+let totalSim nSim = seq {
+    for i in 1..nSim do
         let x = random.NextDouble()
         let y = random.NextDouble()
         if x * x + y * y <= 1.0 then
             yield 1.0
         else 
             yield 0.0
-    }
+}
 
-let totalSum = totalSim |> Seq.sum
-let pi = 4.0 * (float totalSum / float noOfSim)
-
-printfn "Value of estimated Pi %0.5f" pi
+[<EntryPoint>]
+let main argv =
+    if argv.Length = 1 then
+        let noOfSim = argv.[0] |> int
+        let nSim = totalSim noOfSim
+        let totalSum = nSim |> Seq.sum
+        let pi = 4.0 * (float totalSum / float noOfSim)
+        printfn "Value of estimated Pi %0.5f" pi
+        0 // Return 0 to indicate successful execution
+    else
+        printfn "Please specify a valid integer number for simulation"
+        1 // Return 1 to indicate error
